cbd41ef35716d2198ebda0bd41daa829ffab15b1
Calvin Cai <yuncai@microsoft.com>
2024-05-18 14:00:10 +0800

    use WEBSITE_RUN_FROM_PACKAGE instead of zipDeploy

--- logic-app/logic-app-function-app-premium-plan/deploy.ps1
+++ logic-app/logic-app-function-app-premium-plan/deploy.ps1
@@ -1,9 +1,12 @@
 $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $location = "eastus"
-$rg = "lab-logic-app-function-app-premium-plan-03"
+$rg = "lab-logic-app-function-app-premium-plan-05"
 
 az group create -n $rg -l $location
 
 $params = '{\"packageUri\":{\"value\":\"https://mystorage0322.blob.core.windows.net/shared/functionapp-dotnet8-http.zip\"}}'
 az deployment group create --name "demoDeploy-$((Get-Date).ToString('yyyyMMddHHmmss'))" --resource-group $rg --template-file "$scriptPath\main.bicep" --parameters $params --debug 
+
+# Clean up
+# az group delete -n $rg -y --no-wait
